91b25fd7410e0ffc6ac955a865ac9b19289e88ad
Binasa366 <sabinajamolova2003@gmail.com>
2023-03-17 13:21:45 +0500

    ready

---
+++ 64_task/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с 
+//помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int n = ReadInt("Введите число N: ");
+NatNumbers(n);
+
+
+
+void NatNumbers(int number)
+{
+    if (number <= 1)
+    {
+        Console.Write(number);
+    }
+    else
+    {
+        Console.Write(number + ", ");
+        NatNumbers(number - 1);
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
